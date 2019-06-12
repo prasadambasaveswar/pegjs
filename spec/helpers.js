5413db09865a4d666bd657f126f19e4c39cae4d8
Satya Morla <s.satya.sm@gmail.com>
2019-06-12 10:10:31 +0530

    updating files

---
+++ spec/helpers.js
@@ -0,0 +1,3 @@
+if (typeof module !== "undefined") {
+  PEG = require("../lib/peg.js");
+}
---
+++ spec/unit/compiler/passes/report-missing-rules.spec.js
@@ -0,0 +1,13 @@
+describe("compiler pass |reportMissingRules|", function() {
+  var pass = PEG.compiler.passes.check.reportMissingRules;
+
+  it("reports missing rules", function() {
+    expect(pass).toReportError('start = missing', {
+      message:  'Referenced rule "missing" does not exist.',
+      location: {
+        start: { offset:  8, line: 1, column:  9 },
+        end:   { offset: 15, line: 1, column: 16 }
+      }
+    });
+  });
+});
